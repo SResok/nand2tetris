43230db7c76a0eaf1e28b9ac9b19726d5d549ba6
Simon <20287474+SResok@users.noreply.github.com>
2023-04-22 00:46:25 +0200

    Fill.asm completed

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,114 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+@i
+M=0
+
+@SCREEN
+D=A
+
+@addr
+M=D
+
+@fill
+M=0
+
+@filled
+M=0
+
+(LOOP)
+    @KBD
+    D=M
+    @PRESSED
+    D;JGT
+
+    @fill
+    M=0
+
+    @filled
+    D=M
+    @SETUP
+    D;JGT
+
+    @LOOP
+    0;JMP
+
+(PRESSED)
+    @filled
+    D=M
+    @LOOP
+    D;JGT
+
+    @fill
+    M=-1
+
+    @SETUP
+    0;JMP
+
+(SETUP)
+    @filled
+    M=0
+
+    @SCREEN
+    D=A
+    @addr
+    M=D
+
+    @256
+    D=A
+    @remainderCol
+    M=D
+
+(PAINTCOL)
+    @32
+    D=A
+    @remainderRow
+    M=D
+
+    @remainderCol
+    D=M
+    @DONE
+    D;JEQ
+
+    @PAINTROW
+    0;JMP
+    
+(PAINTROW)
+    @remainderRow
+    D=M
+    @ROWDONE
+    D;JEQ
+
+    @fill
+    D=M
+
+    @addr
+    A=M
+    M=D
+    
+    @addr
+    M=M+1
+
+    @remainderRow
+    M=M-1
+    
+    @PAINTROW
+    0;JMP
+
+(ROWDONE)
+    @remainderCol
+    M=M-1
+    @PAINTCOL
+    0;JMP
+
+(DONE)
+    @KBD
+    D=M
+    @LOOP
+    D;JEQ
+
+    @filled
+    M=1
+
+    @LOOP
+    0;JMP
